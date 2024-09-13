2bbb3ff9685ba48c78594d4b2bc4bae5fb174ebf
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-13 14:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// 1. Reference to the players array
+const team = players;
 
+// 2. Copy of the players array
+const team1 = [...players];
+
+// 3. Copy of the person object
+const cap1 = { ...person };
+
+// To expose the variables for the window object (for testing or browser access)
 window.players = players;
 window.person = person;
 window.team = team;
